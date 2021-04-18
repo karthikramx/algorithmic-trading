a806c0823e8dc68c906dd7d0732d706da3800024
Karthik Ram <karthikram570@gmail.com>
2021-04-18 13:14:23 +0530

    Adding slope function

--- techncal_indicators.py
+++ techncal_indicators.py
@@ -177,3 +177,29 @@ def shooting_star(ohlc_df):
                    (abs(df["close"] - df["open"]) > 0.1* (df["high"] - df["low"]))
     return df
 
+def slope(ohlc_df,n):
+    "function to calculate the slope of regression line for n consecutive points on a plot"
+    df = ohlc_df.iloc[-1*n:,:]
+    y = ((df["open"] + df["close"])/2).values
+    x = np.array(range(n))
+    y_scaled = (y - y.min())/(y.max() - y.min())
+    x_scaled = (x - x.min())/(x.max() - x.min())
+    x_scaled = sm.add_constant(x_scaled)
+    model = sm.OLS(y_scaled,x_scaled)
+    results = model.fit()
+    slope = np.rad2deg(np.arctan(results.params[-1]))
+    return slope
+
+def trend(ohlc_df,n):
+    "function to assess the trend by analyzing each candle"
+    df = ohlc_df.copy()
+    df["up"] = np.where(df["low"]>=df["low"].shift(1),1,0)
+    df["dn"] = np.where(df["high"]<=df["high"].shift(1),1,0)
+    if df["close"][-1] > df["open"][-1]:
+        if df["up"][-1*n:].sum() >= 0.7*n:
+            return "uptrend"
+    elif df["open"][-1] > df["close"][-1]:
+        if df["dn"][-1*n:].sum() >= 0.7*n:
+            return "downtrend"
+    else:
+        return None
