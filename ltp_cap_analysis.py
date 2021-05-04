db11f1c49213ed056cf5b3991d949578685bb1d6
Karthik Ram <karthikram570@gmail.com>
2021-05-04 11:44:41 +0530

    Adding extraction code for ltp < 50 and market cap > 2000

--- ltp_cap_analysis.py
+++ ltp_cap_analysis.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
-ltp = pd.read_csv('money_control_ltp.csv')
-
+ltp = pd.read_csv('money_control_ltp.csv', index_col=[0])
+# ltp.sort_index()
 ltp = ltp[~ltp['Company Name'].str.contains("No Data Available")]
-
 ltp.to_csv('money_control_ltpx.csv')
 
-#extract = ltp[ float(ltp["Last Price"]) < 50 ]
+ltp['Last Price'] = ltp['Last Price'].apply(lambda x: float(x))
+ltp['Market Cap(Rs. cr)'] = ltp['Market Cap(Rs. cr)'].apply(lambda x: float(x))
+ltp['% Chg'] = ltp['% Chg'].apply(lambda x: float(x))
+ltp['52 wkHigh'] = ltp['52 wkHigh'].apply(lambda x: float(x))
+ltp['52 wkLow'] = ltp['52 wkLow'].apply(lambda x: float(x))
 
-#extract.to_csv('extract.csv')
 
-print(ltp)
+extract = ltp[ ltp["Last Price"] < 50]
+extract = extract[ extract["Market Cap(Rs. cr)"] > 1000]
+extract.to_csv('extract.csv')
+ltp.to_csv('ltp.csv')
